130177f312eeac10741c542f4a164dab6de4ebe5
jay-kabdwal <jaykabdwal@gmail.com>
2025-07-23 22:35:01 +0530

    aod schema

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,5 +1,7 @@
-import {z} from "zod"
+import { z } from "zod";
 
 export const verifySchema = z.object({
-    code: z.string().length(6,{message:"verificaion code must be 6 characters"})
-})
+  code: z
+    .string()
+    .length(6, { message: "verificaion code must be 6 characters" }),
+});
