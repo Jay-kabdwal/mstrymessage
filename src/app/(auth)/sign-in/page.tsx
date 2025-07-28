664a4afc8b42705e940d2e58e18b5cdc099b7a8d
jay-kabdwal <jaykabdwal@gmail.com>
2025-07-28 11:33:03 +0530

    authjs

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client';
 
-const SignUpPage = () => {
-  return (
-    <div>Sign in Page</div>
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const SignInPage = () => {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <div>Signed in as {session.user?.email}</div>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return(
+    <>
+      <div>Not signed in</div>
+      <button className="bg-amber-600 p-3 m-4 rounded-md" onClick={() => signIn()}>Sign in</button>
+    </>
   )
-}
+};
 
-export default SignUpPage
+export default SignInPage;
