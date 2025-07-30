c4fd3239cd8be6c255d2d6e3e11a021e7102fc9b
jay-kabdwal <jaykabdwal@gmail.com>
2025-07-30 22:30:09 +0530

    get messages

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,68 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request:Request){
+    await dbConnect()
+
+    const session = await getServerSession(authOptions)
+
+    const user: User = session?.user
+    
+    if(!session || !session.user){
+        return Response.json(
+            {
+                success: false,
+                message: "not authenticated",
+            },
+            {
+                status: 401,
+            }
+        );
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+
+    try {
+        const user = await UserModel.aggregate([
+            {$match: {id: userId}},
+            {$unwind:'$messages'},
+            {$sort: {'messages.createdAt': -1}},
+            {$group:{_id:'$_id',messages:{$push:'$messages'}}},
+        ])
+        if(!user || user.length === 0){
+            return Response.json(
+                {
+                    success: false,
+                    message: "user not found",
+                },
+                {
+                    status: 401,
+                }
+            );
+        }
+        return Response.json(
+            {
+                success: true,
+                messages:user[0].messages,
+            },
+            {
+                status: 200,
+            }
+        );
+        
+    } catch (error) {
+        console.error("notfound")
+        Response.json({
+            success: false,
+            message:"notfound"
+        },{
+            status:500
+        })
+    }
+
+
+}
